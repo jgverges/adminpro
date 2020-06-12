7782161ad71844aa9197222eb4f3f4920e0eb2be
GONZAL17 <jordi.gonzalez@kapsch.net>
2020-06-12 21:09:11 +0200

    151

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
 // temporal
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 // servicios 
 import { ServiceModule } from './services/service.module';
 
@@ -25,6 +25,7 @@ import { ServiceModule } from './services/service.module';
     APP_ROUTES,
     FormsModule,
     PagesModule,
+    ReactiveFormsModule,
     ServiceModule
   ],
   providers: [],
--- src/app/login/register.component.ts
+++ src/app/login/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+// declaramos la función de iniciación que hemos creado en asstes/js/custom.js
+declare function init_pluguins();
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  forma: FormGroup;
+
   constructor() { }
 
-  ngOnInit(): void {
+  sonIguales( campo1:string,campo2:string) {
+
+    return ( group: FormGroup) => {
+
+      let pass1 = group.controls[campo1].value;
+      let pass2 = group.controls[campo2].value;
+
+      if ( pass1 === pass2 ) {
+        return null;
+      }
+      
+      return {
+        sonIguales: true
+      }
+
+    }
+  }
+
+
+
+  ngOnInit() {
+    init_pluguins();
+
+    this.forma = new FormGroup({
+      nombre: new FormControl(null, Validators.required ),
+      correo: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl( null, Validators.required),
+      password2: new FormControl( null, Validators.required),
+      condiciones: new FormControl( false ),
+    }, { validators: this.sonIguales ( 'password', 'password2') }  );
+ 
+
+    this.forma.setValue({
+      nombre:'test',
+      correo :'tesr@t',
+      password: '123456',
+      password2: '123456',
+      condiciones: true
+
+    })
+  }
+
+
+  registrarUsuario(){
+
+    if ( this.forma.invalid ){
+      return;
+    }
+
+    if ( !this.forma.value.condiciones){
+      console.log("debe aceptar condiciones");
+      return;
+    }
+
+
+    console.log( this.forma.value);
   }
 
 }
